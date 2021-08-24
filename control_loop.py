6b3c5cc6334181265064fb3d9380c423fbfa3e33
Zachary Kauffman <ztk@iastate.edu>
2021-08-24 16:27:14 -0500

    control_loop updates

--- control_loop.py
+++ control_loop.py
@@ -4,10 +4,11 @@ from controller import SmartController
 
 
 def main(args):
-    controller = SmartController(args.devices)
+    controller = SmartController(args.logfile)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Control a variety of smarthome devices as specified by ~/.ossmarthome/conf')
-    parser.add_argument('--log', help="Log information during the run.")
+    parser.add_argument('--configuration', help="Configuration file to use for SmartController. See README for questions about formatting.")
+    parser.add_argument('--logfile', help="A file to log information to during the run.")
     args = parser.parse_args()
     main(args)
