98ce83fc875f8eed1ae85f7fddc382a7f0747304
Zach Kauffman <z.thekauffman@gmail.com>
2021-09-04 22:55:46 -0500

    Basic configuration yaml. Works with phue lights

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # open-source-smarthome by Zachary Kauffman / ztk@iastate.edu
 
 ## Overview
+
 Amazon Alexa, Google Home, and the wide array of similar products made by the tech giants can make many aspects of our lives more convenient, but at what cost? This project seeks to create an open-source assistant that users can build upon, knowing that they are the only person with access to their data.
 
 ## Installation
--- smart_module.py
+++ smart_module.py
@@ -1,5 +1,6 @@
 import yaml
 import pprint
+import importlib # used to dynamically import from our yaml configurations 
 
 class SmartModule():
     def __init__(self, config_file, caller):
@@ -12,12 +13,19 @@ class SmartModule():
                 self.conf = dictionary
             except yaml.YAMLError as exc:
                 print(exc)
-        pprint.PrettyPrinter(indent=4).pprint(self.__dict__)
-        self.id = self.conf["id"]
+        # To maintain backwards compatability, expose id and address 
+        self.id = self.get_pref("id")
+        self.address = self.get_pref("address")
+        #TODO: shouldn't need to store the return value... figure out why the statement isn't working
+        for to_import in self.get_pref("imports"):
+            print(f"importing {to_import}")
+            self.conf[to_import] = importlib.import_module(to_import)
+        self.execute("setup")
 
     def execute(self, command):
-        self.caller.log(f"Executing '{command}' on '{self.conf['id']}''.")
+        self.caller.log(f"Executing '{command}' on '{self.conf['id']}'.")
         if command in self.get_pref("commands"):
+            exec(self.get_command_actions(command))
             print("Correctly executed!")
         else: 
             print("No matching command found. Please try again.")
@@ -28,3 +36,7 @@ class SmartModule():
         else:
             print("No matching preference found!")
             return None
+
+    def get_command_actions(self, command):
+        if command in self.get_pref("commands"):
+            return self.get_pref("commands")[command]
